09ea2a84a115d5922a22bee5cae39c0593edfcab
ThanhTuan02 <trinhtuan.31032020.com>
2022-08-22 18:32:34 +0700

    baitap

--- module 2/src/s04_class_and_object/baitap/class_stopwatch/endTime.java
+++ module 2/src/s04_class_and_object/baitap/class_stopwatch/endTime.java
@@ -1,4 +1,60 @@
 package s04_class_and_object.baitap.class_stopwatch;
 
+import java.math.BigInteger;
+
 public class endTime {
-}
+
+        public static void main(String[] args) {
+
+            endTime stopwatch1 = new endTime();
+            stopwatch1.start();
+            Fibonacci(45);
+            stopwatch1.stop();
+
+
+            System.out.println("Elapsed time in milliseconds: "
+                    + stopwatch1.getElapsedMilliseconds());
+
+            System.out.println("Elapsed time in seconds: "
+                    + stopwatch1.getElapsedSeconds());
+
+            System.out.println("Elapsed time in minutes: "
+                    + stopwatch1.getElapsedMinutes());
+
+            System.out.println("Elapsed time in hours: "
+                    + stopwatch1.getElapsedHours());
+
+        }
+
+    private String getElapsedHours() {
+        return null;
+    }
+
+    private String getElapsedMinutes() {
+        return null;
+    }
+
+    private String getElapsedSeconds() {
+        return null;
+    }
+
+    private String getElapsedMilliseconds() {
+        return null;
+    }
+
+    private void stop() {
+
+    }
+
+    private void start() {
+
+    }
+
+    private static BigInteger Fibonacci(int n) {
+            if (n < 2)
+                return BigInteger.ONE;
+            else
+                return Fibonacci(n - 1).add(Fibonacci(n - 2));
+        }
+    }
+
